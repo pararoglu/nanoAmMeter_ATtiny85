b009949de4adbb2ec15f77c0e408a4636f57f16a
pararoglu <pararoglu61@gmail.com>
2022-10-25 22:13:56 +0300

    Update NanoCurrentMeter_OLED_SoftSerial.ino

--- NanoCurrentMeter_OLED_SoftSerial.ino
+++ NanoCurrentMeter_OLED_SoftSerial.ino
@@ -1,17 +1,10 @@
-/* ATtiny85 NanoCurrent Meter with OLED
-
-Inspired by
-   David Johnson-Davies - www.technoblogy.com - 9th October 2019
-    http://www.technoblogy.com/show?2S67#cite_note2
-   Also: OneCircuit did a version of this: 
-    https://onecircuit.blogspot.com/2021/05/0000-0000-0101-1111.html
- */
+/* ATtiny85 NanoCurrentMeter 
 
 #include <TinyOzOLED_decimalFix.h> 
 #define voltSwitchPin 1
-
-#include <SoftwareSerial.h> // include library for transmitting the data serially to an external device like a datalogger
-SoftwareSerial tinySerial(-1, 4); // declares tinySerial using D3 for Transmit (tx) but not Receive (pin -1) */
+*/
+#include <SoftwareSerial.h> // bu kütüphane datalogger gibi dış kaynaklı cihazlar ile haberleşme için kullanılabilir
+SoftwareSerial tinySerial(-1, 4); //  tinySerial aygıtının D3 pinin (tx) yapıp RX pinin 1e çekiyoruz*/
 
 // Setup ********************************************** 
 void setup() {
@@ -30,7 +23,7 @@ void loop() {
   pinMode(A3, INPUT);
   unsigned long Start = millis(), Time, nA, kOhms;
   unsigned int Initial = analogRead(A3);
-  int Target = (Initial * 29) / 41; // JD_K: this used to be unsigned, but it gives an error message
+  int Target = (Initial * 29) / 41; // tanımlanmamış olsa da hata mesajı vermektedir.
   
   do {
     Time = millis() - Start;
@@ -40,9 +33,9 @@ void loop() {
   
   if (digitalRead(voltSwitchPin)) {
  // nA = 1732868 / Time; // For 5Volts exactly
- nA = 1739799 / Time; // For 5.02V which is the measured amount of the voltage regulator on this specific device;
+ nA = 1739799 / Time; // bu cihazda kullanılan 7805ten dolayı gelen 5.02V için tanımlanmıştır.;
   OzOled.printString("5V NanoAmmeter", 0,1);
-  kOhms = 5020000/nA; // using the exact 5.02V of this device, and Ohms law to calculate resistance 
+  kOhms = 5020000/nA; // 5.02 V ile ohm yasasının uygulanması 
   } else {
   OzOled.printString("3V3 NanoAmmeter", 0,1); 
   nA = 1143693 / Time;
@@ -55,12 +48,12 @@ void loop() {
   else if (nA < 100000) {  float uA=nA/1000.00; OzOled.printNumber((float)uA,2, 0,3);   OzOled.printString(" uA"); }
   else {   OzOled.printString("HI (over 10uA)", 0,3); }
 
-tinySerial.print(nA); // sends the raw nA value without units to serial Tx (pin D4)
+tinySerial.print(nA); // ham nA verisini birimsiz olarak serial Tx e yollar
 
 OzOled.printString("kOhms= ", 0,5); OzOled.printNumber((long)kOhms);
 
 /*
-// these two lines were for debugging. Very useful
+// buradaki iki satır debug için kullanılmaktadır.
 OzOled.printString("nA= ", 0,5); OzOled.printNumber((long)nA);
 OzOled.printString("Time= ", 0,6); OzOled.printNumber((long)Time);
 */
